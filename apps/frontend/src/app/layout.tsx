154220b33a5f1a4be78b719ec06bd8708ceb8d21
Pedro Lyrio <pedrolyrio@ucl.br>
2024-11-21 16:51:04 -0300

    feito novo header

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Footer} from "@/components/layout/Footer";
-import { Navnav } from "@/components/layout/Navnav"
+import { Header } from "@/components/layout/Header";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -21,7 +21,7 @@ export default function RootLayout({
     <html lang="en">
 
       <body className={inter.className}>
-        <Navnav />
+        <Header />
        <main>{children}</main>
         <Footer />
       </body>
