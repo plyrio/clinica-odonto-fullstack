f7fb558fc2ea102b0fcd4c8b3caca82dab4dda8b
Pedro Lyrio <pedrolyrio@ucl.br>
2025-03-30 13:20:10 -0300

    melhorando pagina dos blogs

--- apps/frontend/src/app/blog/[id]/page.tsx
+++ apps/frontend/src/app/blog/[id]/page.tsx
@@ -1,9 +1,9 @@
-import Hero from "@/components/sections/Hero";
-import { notFound } from "next/navigation";
-import { ResponseBlogPostDto } from "@odonto/core";
-import Section from "@/components/ui/layout/Section";
-import Link from "next/link";
-import Image from "next/image";
+import Hero from '@/components/sections/Hero';
+import { notFound } from 'next/navigation';
+import { ResponseBlogPostDto } from '@odonto/core';
+import Section from '@/components/ui/layout/Section';
+import Link from 'next/link';
+import Image from 'next/image';
 
 
 async function fetchArticle(id: string): Promise<ResponseBlogPostDto | null> {
@@ -12,17 +12,17 @@ async function fetchArticle(id: string): Promise<ResponseBlogPostDto | null> {
       `https://cof-backend.onrender.com/blog-posts/${id}`,
       {
         next: { revalidate: 600 },
-        cache: "force-cache",
+        cache: 'force-cache',
       },
     );
 
     if (!res.ok) {
-      throw new Error("Failed to fetch blog post");
+      throw new Error('Failed to fetch blog post');
     }
 
     return await res.json();
   } catch (error) {
-    console.error("Error fetching article:", error);
+    console.error('Error fetching article:', error);
     return null;
   }
 }
@@ -42,32 +42,50 @@ export default async function ArticlePage({
 
   return (
     <>
-      <Hero title={article.title || "Detalhes do Artigo"} />
+      <Hero title={article.title || 'Detalhes do Artigo'} />
       <Section>
-        
-          <div className="flex flex-col max-w-3xl mx-auto overflow-hidden rounded">
-            <img src={article.imgUrl} alt="" className="h-auto w-full  " />
-            <div className="p-6 pb-12 m-4 mx-auto -mt-16 space-y-6 lg:max-w-2xl sm:px-10 sm:mx-12 lg:rounded-md bg-gray-100">
-              <div className="space-y-2">
-                <Link  href="#" className="inline-block text-2xl font-semibold sm:text-3xl">{article.title || "Título Não Disponível"}</Link>
-                
-                <div  className="flex items-center">  
-                <Image className="rounded-full w-12 items-center flex border-2 border-gray-100 me-2" height={256} width={256} alt="dd" src={article.author?.imgUrl || ""} />
-                  <p className="flex flex-col"> {article.author?.name || "Desconhecido"}
-                  <span className="text-xs">Em: {article.createdAt
-                    ? new Date(article.createdAt).toLocaleDateString("pt-BR")
-                    : "Data indisponível"}</span>
-                  </p>
-                </div>
-              
+        <div className='flex flex-col max-w-3xl mx-auto overflow-hidden rounded'>
+          <Image
+            height={768}
+            width={768}
+            src={article.imgUrl || ""}
+            alt=''
+            className='max-h-96 object-center object-cover w-full  border-4 rounded-2xl'
+          />
+          <div className='p-6 pb-12 m-4 mx-auto -mt-16 space-y-6 lg:max-w-2xl sm:px-10 sm:mx-12 lg:rounded-md bg-gray-100 rounded-2xl'>
+            <div className='space-y-2'>
+              <Link
+                href='#'
+                className='inline-block text-2xl font-semibold sm:text-3xl'>
+                {article.title || 'Título Não Disponível'}
+              </Link>
+              <div className='flex items-center'>
+                <Image
+                  className='rounded-full w-12 items-center flex border-2 border-gray-100 me-2'
+                  height={256}
+                  width={256}
+                  alt='dd'
+                  src={article.author?.imgUrl || ''}
+                />
+                <p className='flex flex-col'>
+                  {article.author?.name || 'Desconhecido'}
+                  <span className='text-xs'>
+                    {' '}
+                    {article.createdAt
+                      ? new Date(article.createdAt).toLocaleDateString('pt-BR')
+                      : 'Data indisponível'}
+                  </span>
+                </p>
               </div>
-              <div className="">
-                <p>{article.content || "Conteúdo não disponível"}</p>
+            </div>
+            <div className=''>
+              <p>{article.content || 'Conteúdo não disponível'}</p>
+              <div className='flex items-center  space-x-3'>
+                
               </div>
             </div>
           </div>
-      
-
+        </div>
 
         {/* <h1>{article.title || "Título Não Disponível"}</h1>
       <p>Autor: {article.author?.name || "Desconhecido"}</p>
@@ -82,7 +100,6 @@ export default async function ArticlePage({
         <p>{article.content || "Conteúdo não disponível"}</p>
       </div> */}
       </Section>
-      
     </>
   );
 }
--- apps/frontend/src/components/ui/blog/layout/ArticleCard.tsx
+++ apps/frontend/src/components/ui/blog/layout/ArticleCard.tsx
@@ -6,58 +6,67 @@ import { ptBR } from 'date-fns/locale';
 import { IconEye, IconThumbUp } from '@tabler/icons-react';
 
 interface ArticlesCardProps {
-    article: ResponseBlogPostDto
+  article: ResponseBlogPostDto;
 }
 
-
 export default function ArticleCard({ article }: ArticlesCardProps) {
-    return (
-        <div key={article.id} className="overflow-hidden rounded-lg shadow-sm transition hover:shadow-lg ">
-            <Link href={`/blog/${article.id}`}>
-                <Image
-                    height={528}
-                    width={528}
-                    alt="Thumbnail"
-                    className="h-56 w-full object-cover"
-                    src={article.imgUrl || 'Imagem não disponivel'}
-                />
-            </Link>
-            <div className="p-4 sm:p-6">
-                <Link href={`/blog/${article.id}`} className="font-medium hover:underline">
-                    {article.title}
-                </Link>
-                <div className="flex flex-col space-y-2 lg:flex-row items-center justify-between mt-2">
-
-                    <div className="flex h-full items-center">
-                        <Image className="rounded-full w-12 items-center flex border-2 border-gray-100 me-2" height={256} width={256} alt="dd" src={article.author?.imgUrl || ""} />
-                        <p className="flex flex-col text-xs text-gray-400"> {article.author?.name || "Desconhecido"}
-                            <span className="text-xs text-gray-400">{formatDistanceToNow(new Date(article.createdAt ?? 'Data indisponivel'), { addSuffix: true, locale: ptBR })}</span>
-                        </p>
-                    </div>
-
-
-
-                    <div className='flex items-center  space-x-3'>
-                        <div className='flex items-center'>
-                            <IconThumbUp stroke={1} className="flex text-xs text-gray-400" />
-                            <span className="text-sm text-gray-400 flex  ">
-                                {article.likes ?? 0}
-                            </span>
-                        </div>
-
-                        <div className='flex items-center'>
-                            <IconEye stroke={1} className="flex text-xs text-gray-400" />
-                            <span className="flex text-xs text-gray-400">
-                                {article.views ?? 0}
-                            </span>
-                        </div>
-
-
-                    </div>
-
-                </div>
-
+  return (
+    <div
+      key={article.id}
+      className='overflow-hidden rounded-lg shadow-card transition hover:shadow-lg '>
+      <Link href={`/blog/${article.id}`}>
+        <Image
+          height={528}
+          width={528}
+          alt='Thumbnail'
+          className='h-56 w-full object-cover'
+          src={article.imgUrl || 'Imagem não disponivel'}
+        />
+      </Link>
+      <div className='p-4 sm:p-6 w-full'>
+        <Link
+          href={`/blog/${article.id}`}
+          className='font-medium hover:underline'>
+          {article.title}
+        </Link>
+        <div className='flex flex-wrap space-y-2  items-center justify-between mt-2 w-full h-full'>
+          <div
+            className='flex
+      h-full items-center'>
+            <Image
+              className='rounded-full object-cover h-12 w-12 items-center flex border-2 border-gray-100 me-2'
+              height={256}
+              width={256}
+              alt='dd'
+              src={article.author?.imgUrl || ''}
+            />
+            <p className='flex flex-col text-xs text-gray-400'>
+              {article.author?.name || 'Desconhecido'}
+              <span className='text-xs text-gray-400'>
+                {formatDistanceToNow(
+                  new Date(article.createdAt ?? 'Data indisponivel'),
+                  { addSuffix: true, locale: ptBR },
+                )}
+              </span>
+            </p>
+          </div>
+          <div className='flex items-center justify-center  space-x-3'>
+            <div className='flex items-center'>
+              <IconThumbUp stroke={1} className='flex text-xs text-gray-400' />
+              <span className='text-sm text-gray-400 flex  '>
+                {article.likes ?? 0}
+              </span>
             </div>
+
+            <div className='flex items-center'>
+              <IconEye stroke={1} className='flex text-xs text-gray-400' />
+              <span className='flex text-xs text-gray-400'>
+                {article.views ?? 0}
+              </span>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
